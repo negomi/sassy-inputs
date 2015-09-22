aeb86b4557b8f41a57653ee1e40f936988ef8a03
Imogen <imogen.wentworth@gmail.com>
2015-09-21 17:51:48 -0700

    Remove autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
-var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
   gulp.src('demo.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
     .pipe(gulp.dest(''))
     .pipe(connect.reload());
 });
